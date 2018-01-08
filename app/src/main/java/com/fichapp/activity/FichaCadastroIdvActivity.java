d53945764b1622065ea8815dfd6f77775650319d
rodrigocosta <rodrigo.costa.ssa@gmail.com>
2018-01-08 20:02:31 -0300

    .

---
+++ app/src/main/java/com/fichapp/activity/FichaCadastroIdvActivity.java
@@ -0,0 +1,21 @@
+package com.fichapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fichapp.R;
+
+public class FichaCadastroIdvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ficha_cadastro_idv);
+
+
+
+    }
+
+
+
+}
